18d30c93107818f510404312750afa68754891a4
Joachim Mårtensson <joachimmartensson@gmail.com>
2010-08-09 00:11:56 +0200

    Added specs for If

--- spec/if_spec.rb
+++ spec/if_spec.rb
@@ -6,3 +6,20 @@ describe "If.new" do
     pattern.pattern.should be_an_instance_of(Pegarus::Character)
   end
 end
+
+describe "If#match" do
+  it "returns nil if the first pattern does not match" do
+    pat = Pegarus.pattern(1) + +Pegarus.pattern("a")
+    pat.match("").should be_nil
+  end
+
+  it "returns nil if the second pattern does not match" do
+    pat = Pegarus.pattern(1) + +Pegarus.pattern("c")
+    pat.match("ab").should be_nil
+  end
+
+  it "returns the index of the character following the matched substring" do
+    pat = Pegarus.pattern(1) + +Pegarus.pattern("ac")
+    pat.match("aacbde").should == 1
+  end
+end
