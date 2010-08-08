2cf62790dff15c09be840cfa2d089c019b51b7e4
Joachim Mårtensson <joachimmartensson@gmail.com>
2010-08-08 23:01:34 +0200

    Added specs for Choice#match
    Signed-off-by: Brian Ford <brixen@gmail.com>

--- spec/choice_spec.rb
+++ spec/choice_spec.rb
@@ -7,3 +7,20 @@ describe "Choice.new" do
     pattern.second.should be_an_instance_of(Pegarus::Character)
   end
 end
+
+describe "Choice#match" do
+  it "returns nil if none of the patterns match" do
+    pat = Pegarus.pattern("a") / Pegarus.pattern("b")
+    pat.match("c").should be_nil
+  end
+
+  it "matches the first pattern if both match" do
+    pat = Pegarus.pattern("aaa") / Pegarus.pattern("a")
+    pat.match("aaa").should == 3
+  end
+  
+  it "matches the second pattern if the first does not match" do
+    pat = Pegarus.pattern("a") / Pegarus.pattern("bc")
+    pat.match("bc").should == 2
+  end
+end
