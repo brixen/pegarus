1fc0fbb437588a8df33911257d036fc315f0b3fa
Joachim Mårtensson <joachimmartensson@gmail.com>
2010-08-09 00:12:15 +0200

    added specs for Unless

--- spec/unless_spec.rb
+++ spec/unless_spec.rb
@@ -6,3 +6,21 @@ describe "Unless.new" do
     pattern.pattern.should be_an_instance_of(Pegarus::Character)
   end
 end
+
+describe "Unless#match" do
+  it "returns nil if the first pattern does not match" do
+    pat = Pegarus.pattern(1) + -Pegarus.pattern("a")
+    pat.match("").should be_nil
+  end
+
+  it "returns nil if the second pattern match" do
+    pat = Pegarus.pattern(1) + -Pegarus.pattern("b")
+    pat.match("ab").should be_nil
+  end
+
+  it "returns the index of the character following the matched substring" do
+    pat = Pegarus.pattern(1) + -Pegarus.pattern("xx")
+    pat.match("aacbde").should == 1
+  end
+end
+
