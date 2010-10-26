1e3289df877e8322e3dff87d743f8ad3518623e7
Brian Ford <brixen@gmail.com>
2010-10-26 10:07:22 -0700

    Make interim version explicit.

--- lib/pegarus/version.rb
+++ lib/pegarus/version.rb
@@ -1,3 +1,14 @@
 module Pegarus
-  VERSION = "0.3.0"
+  module VERSION
+    MAJOR = 0
+    MINOR = 3
+    TINY  = 0
+    BUILD = 'dev'
+
+    STRING = [MAJOR, MINOR, TINY, BUILD].join('.')
+  end
+
+  def VERSION.to_s
+    self::STRING
+  end
 end
