9efbac6bee6900eaffcc82878d0a7ac38b2bc8ef
Brian Shirai <brixen@gmail.com>
2014-01-11 21:11:03 -0800

    Updated version.rb.

--- lib/pegarus/version.rb
+++ lib/pegarus/version.rb
@@ -1,14 +1,3 @@
 module Pegarus
-  module VERSION
-    MAJOR = 0
-    MINOR = 3
-    TINY  = 0
-    BUILD = 'dev'
-
-    STRING = [MAJOR, MINOR, TINY, BUILD].join('.')
-  end
-
-  def VERSION.to_s
-    self::STRING
-  end
+  VERSION = "0.3.0.dev"
 end
